27ec402ddb284cb8b0dd16ee07b66ac9f62d5391
moe <mmckinnon@comprofix.com>
2015-01-05 20:58:51 +1000

    Updated diskalert.sh with correct size 95%

--- diskalert.sh
+++ diskalert.sh
@@ -5,7 +5,7 @@ devnames=`df -h | grep "/dev/" | awk '{print $1}'`
 for devname in $devnames
     do
         let p=`df -Pk $devname | grep -v ^File | awk '{printf ("%i", $5) }'`
-        if [ $p -ge 1 ] 
+        if [ $p -ge 90 ] 
         then
             df -h $devname > /dev/null
             echo "Running out of space \"$devname ($p%)\" on $(hostname) as on $(date)" 
