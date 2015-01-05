d7b2ca573266430121aa8b4bc98ebd0856e334ac
moe <mmckinnon@comprofix.com>
2015-01-05 20:56:15 +1000

    Updated diskalert to scan /dev/

--- diskalert.sh
+++ diskalert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source /etc/profile
 
-devnames="/dev/md0 / /dev/sdc1"
+devnames=`df -h | grep "/dev/" | awk '{print $1}'`
 for devname in $devnames
     do
         let p=`df -Pk $devname | grep -v ^File | awk '{printf ("%i", $5) }'`
-        if [ $p -ge 95 ] 
+        if [ $p -ge 1 ] 
         then
             df -h $devname > /dev/null
             echo "Running out of space \"$devname ($p%)\" on $(hostname) as on $(date)" 
