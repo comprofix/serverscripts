14771ab147bef32fda9efcc8234e76acaba0f1b1
Matt McKinnon <mmckinnon@comprofix.com>
2016-09-26 22:53:01 +1000

    Updated sentora_www-backup.sh - creates single tar.gz file of websites

--- sentora_www-backup.sh
+++ sentora_www-backup.sh
@@ -7,8 +7,10 @@
 #   Backup your public_html folder under /var/sentora/hostdata/DOMAIN_NAME/public_html
 
 
-#find domains
+WEBPATH=$(find /var/sentora/hostdata/* -maxdepth 0 -type d  | awk -F/ '{print $5}')
 
-find /var/sentora/hostdata/* -maxdepth 0 -type d  | awk -F/ '{print $5}'
+for SITES in $WEBPATH; do
+    tar -zcvf /BACKUP/www/$SITES.tar.gz /var/sentora/hostdata/$SITES
+done
 
 
